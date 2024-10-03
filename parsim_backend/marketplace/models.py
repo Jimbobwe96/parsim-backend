224400bd69c3b168d1c137854e55e24874a3127c
Jimmy Mayer <jimmy.mayer.23@gmail.com>
2024-10-02 21:52:58 -0400

    created django project

---
+++ parsim_backend/marketplace/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Listing(models.Model):
+    seller = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    image = models.ImageField(upload_to='listings/')
+    is_sold = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
