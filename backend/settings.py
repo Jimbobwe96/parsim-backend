a4f2bde7583e42dda811e4ac9f023397e0f634b0
Jimmy Mayer <jimmy.mayer.23@gmail.com>
2024-10-24 01:13:18 -0400

    mysql migrations made, data model generated RAH

--- backend/settings.py
+++ backend/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'parsimdb',
+        'USER': 'root',
+        'PASSWORD': 'Ilive@5680',
+        'HOST': '127.0.0.1',
+        'PORT': '3308'
     }
 }
 
